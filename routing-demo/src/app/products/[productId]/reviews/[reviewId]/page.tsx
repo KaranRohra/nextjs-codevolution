5009b2ae6e261233a54cdd12e3ddc84c92af7933
KaranRohra <rohrakaran38@gmail.com>
2024-06-24 10:01:53 +0530

    24 - error.tsx

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,20 @@
 import { notFound } from "next/navigation";
 import React from "react";
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 const ProductReview = ({ params }: { params: Record<string, string> }) => {
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
+
   if (parseInt(params.reviewId) > 1000) {
     return notFound();
   }
+
   return (
     <div>
       <h1>
