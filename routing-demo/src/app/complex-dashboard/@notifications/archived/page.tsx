a490c2551aae5017b7d0f0bcc9f9c50edb5e1882
KaranRohra <rohrakaran38@gmail.com>
2024-07-06 18:57:54 +0530

    29 - Unmatched Routes

--- routing-demo/src/app/complex-dashboard/@notifications/archived/page.tsx
+++ routing-demo/src/app/complex-dashboard/@notifications/archived/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Notifications() {
   return (
     <Card>
-      Notifications
-      <Link href="/complex-dashboard/archived">Archived</Link>
+      Archived Notification
+      <Link href="/complex-dashboard">Default</Link>
     </Card>
   );
 }
