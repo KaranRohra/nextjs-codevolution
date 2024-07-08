ef92feae7517328313e3bd79bc745c2a5163af81
KaranRohra <rohrakaran38@gmail.com>
2024-07-08 17:40:33 +0530

    37 - Handling PATCH Request

--- route-handlers-demo/src/app/todos/[id]/route.ts
+++ route-handlers-demo/src/app/todos/[id]/route.ts
@@ -8,3 +8,22 @@ export async function GET(
     todos.find((todo) => todo.id.toString() === params.id) || {}
   );
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const todoIndex = todos.findIndex((todo) => todo.id.toString() === params.id);
+
+  if (todoIndex === -1)
+    return new Response(JSON.stringify({ message: "Not found" }), {
+      status: 404,
+    });
+
+  todos[todoIndex] = {
+    ...todos[todoIndex],
+    ...((await request.json()) || {}),
+  };
+
+  return Response.json(todos[todoIndex]);
+}
