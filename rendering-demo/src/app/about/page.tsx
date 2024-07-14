b49dd3929ba83ec85336d0ddff8a47fc8a1283da
KaranRohra <rohrakaran38@gmail.com>
2024-07-14 10:22:34 +0530

    53 - Dynamic Rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,6 +1,12 @@
+import { cookies } from "next/headers";
 import React from "react";
 
+// Dynamic Rendering automatically enables when searchParams, cookies or headers are used in component.
 const page = () => {
+  const cookieStore = cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme);
+
   console.log("About Server Component");
 
   return <h1>About {new Date().toLocaleTimeString()}</h1>;
