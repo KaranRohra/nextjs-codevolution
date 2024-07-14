3bec87cca7f54edadad00f87bdac90903b67204d
KaranRohra <rohrakaran38@gmail.com>
2024-07-14 13:27:38 +0530

    58 - Context Providers

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -1,8 +1,15 @@
 "use client";
 import ImageCarousel from "@/components/ImageCarousel";
+import { useTheme } from "@/components/theme-provider";
 
 const ClientRoutePage = () => {
-  return <ImageCarousel />;
+  const theme = useTheme();
+  return (
+    <div>
+      <h1 style={{ color: theme.colors.primary }}>Hello</h1>
+      <ImageCarousel />
+    </div>
+  );
 };
 
 export default ClientRoutePage;
