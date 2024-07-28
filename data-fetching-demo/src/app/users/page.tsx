a44fc05039a926a094428189df08df7c1bb6b43e
KaranRohra <rohrakaran38@gmail.com>
2024-07-28 09:47:04 +0530

    64 - Loading and Errors States

--- data-fetching-demo/src/app/users/page.tsx
+++ data-fetching-demo/src/app/users/page.tsx
@@ -9,7 +9,10 @@ type User = {
 };
 
 const UsersPage = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  // To show error.tsx content uncomment this
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users1");
   const users = await response.json();
 
   return (
