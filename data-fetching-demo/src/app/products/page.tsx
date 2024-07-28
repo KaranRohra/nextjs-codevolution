c0403a825fe8afd1b2fd12f82718e59c5c6bfacb
KaranRohra <rohrakaran38@gmail.com>
2024-07-28 10:46:57 +0530

    68 - Opting Out of Caching

--- data-fetching-demo/src/app/products/page.tsx
+++ data-fetching-demo/src/app/products/page.tsx
@@ -1,13 +1,29 @@
+// This will make sure only no-store fetch calls are not get cached.
+// export const fetchCache = "default-cache";
+
+import { cookies } from "next/headers";
+
 type Product = {
   id: number;
   title: string;
   price: number;
   description: string;
 };
+export default async function ProductsPage(props: any) {
+  const qs = props.searchParams;
 
-const ProductsPage = async () => {
+  // const res = await fetch("http://localhost:3001/products", {
+  //   cache: "no-store", // By adding this all subsequent api call will not get cached.
+  // });
+  // const products = await res.json();
+  // const detailsResponse = await fetch("http://localhost:3001/products/1");
+  // const details = await detailsResponse.json();
+
   const res = await fetch("http://localhost:3001/products");
   const products = await res.json();
+  cookies();
+  const detailsResponse = await fetch("http://localhost:3001/products/1");
+  const details = await detailsResponse.json();
 
   return (
     <ul className="space-y-4 p-4">
@@ -19,11 +35,9 @@ const ProductsPage = async () => {
           <h2 className="text-xl font-semibold">{product.title}</h2>
           <p>{product.description}</p>
           <p className="text-lg font-medium">${product.price}</p>
-          {/* <p>{details.title}</p> */}
+          <p>{details.title}</p>
         </li>
       ))}
     </ul>
   );
-};
-
-export default ProductsPage;
+}
